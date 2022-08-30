8886013cde2c68f647759ea8a7bc0ada39597049
Austin Gentz <austingentz@icloud.com>
2022-08-30 01:48:41 -0700

    created the REACT application, installed Tailwind, Created routes using react-router-dom.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	theme: {
+		extend: {
+			spacing: {
+				128: '32rem',
+				144: '36rem',
+			},
+			borderRadius: {
+				'4xl': '2rem',
+			},
+		},
+	},
+};
